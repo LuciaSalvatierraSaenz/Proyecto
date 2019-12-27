ecf928493026bc8135b1a10c27c4915e44cf0a54
LuciaSalvatierraSaenz <59099156+LuciaSalvatierraSaenz@users.noreply.github.com>
2019-12-27 15:15:46 +0100

    hola
    h

---
+++ js/functions.js
@@ -0,0 +1,40 @@
+function VentanaAlerta()
+{
+    alert("ATENCIÓN VENTANA DE ALERTA");
+}
+
+
+function VentanaConfirmacion(){
+var respuesta;
+   
+    if(confirm("¿Desea Confirmar?"))
+    {
+        respuesta="Has pulsado OK";
+    }
+    
+    else
+    {
+        respuesta= "Has pulsado CANCEL";
+    }
+    
+    document.getElementById("MensajeConfirmacion").innerHTML= respuesta;
+}
+
+
+function PideInformacion(){
+var respuesta;
+    
+    var person= prompt("¿Cómo te llamas?");
+    
+    if(person== null || person=="")
+    {
+        respuesta="El usuario ha pulsado CANCEL";
+    }
+
+    else
+    {
+        respuesta="Hola" + " " + person + ". " + "¿Qué tal te va la vida?";
+    }
+
+    document.getElementById("MensajeInformacion").innerHTML= respuesta;
+}
